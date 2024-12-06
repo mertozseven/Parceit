5c36db2ec0354d09a689a3d2fc8e50583f649610
Mert Adem Özseven <mertozseven@outlook.com>
2024-12-06 14:05:30 +0300

    Configured MVVM folder structure

---
+++ Parceit/Parceit/Features/Home/Views/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Parceit
+//
+//  Created by Mert Ozseven on 6.12.2024.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    // MARK: - Private Methods
+    private func configureUI() {
+        view.backgroundColor = .systemBackground
+        addViews()
+        configureLayout()
+    }
+    
+    private func addViews() {
+        
+    }
+    
+    private func configureLayout() {
+        
+    }
+
+}
--- Parceit/Parceit/Utilities/SceneDelegate.swift
+++ Parceit/Parceit/Utilities/SceneDelegate.swift
@@ -11,12 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         self.window = window
-        let navigationController = UINavigationController(rootViewController: ViewController())
+        let navigationController = UINavigationController(rootViewController: HomeViewController())
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
     }
